353a7bdbecfbe5cbc9a4df219fc98b8ec9cf81b8
Imyourname <87386437+Imyourname@users.noreply.github.com>
2021-07-26 21:08:11 +0530

    Add files via upload

---
+++ c9-STUD-TEACHER-main/c9-STUD-TEACHER-main/sketch.js
@@ -0,0 +1,22 @@
+
+var b = prompt('Enter the first variable:') ;
+var a = prompt('Enter the first variable: ');
+
+function setup(){
+   createCanvas(400,400);
+  var b2 = createButton("click here to Swap");
+  b2(mousePressed) (swap) ;
+  }
+
+
+function draw() {
+  background(220) ;
+
+}
+
+function Swap ()
+{
+   [a,b] = [b,a] ;
+  
+   console.log("The value of a after swapping:",+ a) ;
+   console.log("The value of b after swapping :",+b) ;
